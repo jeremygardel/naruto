dc4702d1e428ef361229605c6bb05d8b9720e0c1
jeremygardel <jeremy-asklany@proton.me>
2023-06-01 16:09:55 +0300

    Mp4 video, Login tap and python

--- js/script.js
+++ js/script.js
@@ -30,7 +30,45 @@ function searchArray() {
     }
 }
 
+//      Login Page Pop Up
 
+let overlay = document.getElementById("overlayid");
+let loginPage = document.getElementById("loginid");
+let signIn = document.getElementById("link");
+let popexit = document.getElementById("popexit");
+let btnlogin = document.getElementById("btnlogin");
+let email = document.getElementById("loginemail");
+let password = document.getElementById("loginpassword");
+
+
+$(document).ready(function () {
+    $('.overlay').hide();
+    $('.login-page').hide();
+    $('#link').click(function () {
+        $('.overlay').fadeIn(1000);
+        $('.login-page').fadeIn(1000);
+    });
+});
+$(document).ready(function () {
+    $('#popexit').click(function() {
+        $('.overlay').fadeOut(1000);
+        $('.login-page').fadeOut(1000);
+    });
+});
+
+
+function exitLogin() {
+    // overlay.style.display = "none";
+    // loginPage.style.display = "none";
+}
+function login() {
+    if(email.value && password.value === "") {
+    }
+    else if(emial.value.contains('@')) {
+        overlay.style.display = "none";
+        loginPage.style.display = "none";
+    }
+}
 
 // >>> NEED TO BE EDITED!
 // Responsive Navbar
